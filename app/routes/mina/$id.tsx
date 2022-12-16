4660fe457cfadcc9cd2223db190480157898e9a7
Xen <1435589+xendarboh@users.noreply.github.com>
2022-12-16 06:16:50 +0000

    feat: add CORS headers for local dev

--- app/routes/mina/$id.tsx
+++ app/routes/mina/$id.tsx
@@ -4,9 +4,18 @@ import invariant from "tiny-invariant";
 
 import { getCachedBioAuth } from "~/mina.server";
 
+// CORS
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+};
+
 export async function loader({ params }: LoaderArgs) {
   invariant(params.id, "id required");
   const bioAuth = await getCachedBioAuth(params.id);
-  if (!bioAuth) throw new Response("Not Found", { status: 404 });
-  return json(bioAuth);
+  if (!bioAuth)
+    throw new Response("Not Found", {
+      status: 404,
+      headers,
+    });
+  return json(bioAuth, { headers });
 }
